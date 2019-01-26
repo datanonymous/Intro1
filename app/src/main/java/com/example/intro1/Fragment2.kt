ceb7ee2fc08c0183625a38391362d5d1e45bf640
Alex Ko <ko.e.alexander@gmail.com>
2019-01-26 18:54:19 -0500

    123

---
+++ app/src/main/java/com/example/intro1/Fragment2.kt
@@ -0,0 +1,14 @@
+package com.example.intro1
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.activity_main.*
+
+class Fragment2: Fragment(){
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment2xml,appbar,false)
+    }
+}
---
+++ app/src/main/java/com/example/intro1/Fragment3.kt
@@ -0,0 +1,14 @@
+package com.example.intro1
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.activity_main.*
+
+class Fragment3: Fragment(){
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment3xml,appbar,false)
+    }
+}
